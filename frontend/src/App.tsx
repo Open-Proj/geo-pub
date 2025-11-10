6c984273831a94a09dbfa993128c40813ea969bf
Noah Huppert <contact@noahh.io>
2025-11-09 20:58:55 -0500

    make map work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Map } from './components/Map/Map';
 
-export function App() {
+export default function App() {
   return (
     <Map />
   );
--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,24 +1,30 @@
 import "leaflet/dist/leaflet.css";
-import "leaflet/dist/leaflet.js";
-
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
-
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export function Map() {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {/* <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-                </Marker> */}
-        </MapContainer>
+        <div
+            style={{
+                height: "100vh",
+                width: "100%",
+            }}
+        >
+            <MapContainer
+                center={[51.505, -0.09]}
+                zoom={13}
+                scrollWheelZoom={true}
+                style={{ height: "100%", width: "100%" }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 }
